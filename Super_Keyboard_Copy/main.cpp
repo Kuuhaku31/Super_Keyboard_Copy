a3fdd9b635ae9c8abe26e3e47f2a2749207f9bb5
Kuuhaku13 <SP_YYY@outlook.com>
2024-04-05 14:56:14 +0800

    roop

--- Super_Keyboard_Copy/main.cpp
+++ Super_Keyboard_Copy/main.cpp
@@ -9,9 +9,6 @@
 #include <conio.h>
 #include <iostream>
 
-std::wifstream file(L"./test.txt"); // 替换为你的文件路径
-std::vector<wchar_t> charArray;
-
 void 
 SendUnicodeChar(wchar_t c)
 {
@@ -37,15 +34,16 @@ SendUnicodeChar(wchar_t c)
 }
 
 void
-fun_01()
+Copy()
 {
-    setlocale(LC_ALL,"chs"); // 设置locale为中文(简体)
+    std::vector<wchar_t> charArray;
+    std::wifstream file(L"./test.txt"); // 替换为你的文件路径
     file.imbue(std::locale("")); // 使用用户的默认locale
-
+    
     if(file.is_open())
     {
         wchar_t c;
-        while (file.get(c)) { charArray.push_back(c); }
+        while(file.get(c)) { charArray.push_back(c); }
         file.close();
     }
     else
@@ -53,7 +51,7 @@ fun_01()
         std::cout<<"error\n";
         return;
     }
-    
+
     std::cout<<"按下Enter开始抄写...\n";
     while(!(GetKeyState(VK_RETURN)&0x8000));
     while(GetKeyState(VK_RETURN)&0x8000);
@@ -64,10 +62,31 @@ fun_01()
         std::wcout<<c;
         SendUnicodeChar(c);
         if((GetKeyState(VK_SPACE)&0x8000)) { break; }
-        Sleep(30);
+        Sleep(1);
     }
 
     std::cout<<"\n==========================================================\n抄写结束\n";
+
+}
+
+void
+fun_01()
+{
+    setlocale(LC_ALL,"chs"); // 设置locale为中文(简体)
+
+    char i = 0;
+    while('q'!=i)
+    {
+        std::cout<<"按下'N'键开始读取\n按下'Q'键退出...\n";
+        i = 0;
+        i = _getch();
+        if('n'==i)
+        {
+            Copy();
+        }
+    }
+
+    std::cout<<"程序已退出：）\n";    
 }
 
 int
