6d4b8ca460fdb71a4541a0d0a5e1b2acd8fc562a
Kuuhaku13 <SP_YYY@outlook.com>
2024-04-10 12:33:23 +0800

    Mcopy

--- Note/Note.cpp
+++ Note/Note.cpp
@@ -1,31 +1,36 @@
 ﻿
+#include <math.h> 
 #include <stdio.h>
-#include <math.h>
 
-const float PI = 3.14;
-
-int inv(int m)
+float bmi(float height, float weight)
 {
-    int result = 0;
-    for (int i = 0; i < 5; ++i)
-    {
-        result = result * 10 + m % 10;
-        m /= 10;
-    }
-    return result;
+    return weight/(height*height);
 }
 
 int main()
 {
-    int i_a = 0, i_b = 0, i_c = 0, i_d = 0, i_e = 0, i_f = 0, i_g = 0, io1 = 0, io2 = 0; float f_a = 0, f_b = 0, f_c = 0, f_d = 0, f_e = 0, f_f = 0, f_g = 0, fo1 = 0, fo2 = 0;
-//  scanf("%d %d %d %d %d %d %d",&i_a,&i_b,&i_c,&i_d,&i_e,&i_f,&i_g);
-    scanf("%f %f %f %f %f %f %f", &f_a, &f_b, &f_c, &f_d, &f_e, &f_f, &f_g);
+    float height, weight, bmiValue;
 
-    fo1 = f_a*f_b+f_c;
+    scanf("%f %f", &height, &weight);
 
-    printf("实发工资为%.2f", fo1);
-//  printf("%.2f%.2f", o1, o2);
+    bmiValue = bmi(height, weight);
+
+    if(bmiValue<18.5)
+    {
+        printf("身体质量偏瘦");
+    }
+    else if(bmiValue<24)
+    {
+        printf("身体质量正常");
+    }
+    else if(bmiValue<28)
+    {
+        printf("身体质量偏胖");
+    }
+    else
+    {
+        printf("身体质量肥胖");
+    }
 
     return 0;
 }
-
--- Super_Keyboard_Copy/main.cpp
+++ Super_Keyboard_Copy/main.cpp
@@ -1,13 +1,9 @@
 
 #include <windows.h>
-#include <winuser.h>
-
-#include <ShlObj.h>
+#include <iostream>
 #include <fstream>
 #include <vector>
-#include <locale>
 #include <conio.h>
-#include <iostream>
 
 void 
 SendUnicodeChar(wchar_t c)
@@ -16,19 +12,20 @@ SendUnicodeChar(wchar_t c)
     ZeroMemory(&input, sizeof(INPUT));
     input.type = INPUT_KEYBOARD;
 
-    if('\n'==c)
-    {
-        input.ki.wVk = VK_RETURN;
-    }
-    else if('\t'==c)
-    {
-        input.ki.wVk = VK_TAB;
-    }
-    else
-    {
-        input.ki.wScan = c;
-        input.ki.dwFlags = KEYEVENTF_UNICODE;
-    }
+    input.ki.wScan = c;
+    input.ki.dwFlags = KEYEVENTF_UNICODE;
+
+    SendInput(1, &input, sizeof(INPUT));
+}
+
+void
+SendVKCode(wchar_t c)
+{
+    INPUT input;
+    ZeroMemory(&input, sizeof(INPUT));
+    input.type = INPUT_KEYBOARD;
+
+    input.ki.wVk = c;
 
     SendInput(1, &input, sizeof(INPUT));
 }
@@ -66,6 +63,60 @@ Copy()
     }
 
     std::cout<<"\n==========================================================\n抄写结束\n";
+                
+}
+
+// 适应读取功能
+void
+MCopy()
+{
+    std::vector<wchar_t> charArray;
+    std::wifstream file(L"./test.txt"); // 替换为你的文件路径
+    file.imbue(std::locale("")); // 使用用户的默认locale
+
+    if(file.is_open())
+    {
+        wchar_t c;
+        while(file.get(c)) { charArray.push_back(c); }
+        file.close();
+    }
+    else
+    {
+        std::cout<<"error\n";
+        return;
+    }
+
+    std::cout<<"按下Enter开始适应抄写...\n";
+    while(!(GetKeyState(VK_RETURN)&0x8000));
+    while(GetKeyState(VK_RETURN)&0x8000);
+    std::cout<<"==========================================================\n";
+
+    for(wchar_t c : charArray)
+    {
+        std::wcout<<c;
+        SendUnicodeChar(c);
+        if((GetKeyState(VK_SPACE)&0x8000)) { break; }
+        Sleep(1);
+
+        switch(c)
+        {
+        case '{':
+        case '[':
+        case '(':
+            SendVKCode(VK_DELETE); 
+            break;
+
+        case '\n': 
+            SendUnicodeChar(' ');
+            SendVKCode(VK_RETURN);
+            SendVKCode(VK_HOME);
+            break;
+        
+        default: break;
+        }
+    }
+
+    std::cout<<"\n==========================================================\n抄写结束\n";
 
 }
 
@@ -77,13 +128,17 @@ fun_01()
     char i = 0;
     while('q'!=i)
     {
-        std::cout<<"按下'N'键开始读取\n按下'Q'键退出...\n";
+        std::cout<<"按下'N'键开始读取\n按下'M'键开始适应读取\n按下'Q'键退出...\n";
         i = 0;
         i = _getch();
         if('n'==i)
         {
             Copy();
         }
+        else if('m'==i)
+        {
+            MCopy();
+		}
     }
 
     std::cout<<"程序已退出：）\n";    
